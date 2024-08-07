6f3f56b529732840b468e1cb22550d92d538020b
Jimbruttancalicut <ppbott91@gmail.com>
2024-08-07 11:40:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -63,7 +63,7 @@ global.mess = {
     error: 'Error!',
 }
 
-global.thumb = fs.readFileSync('./XeonMedia/thumb.jpg')
+global.thumb = fs.readFileSync('./XeonMedia/Menu.jpg')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
